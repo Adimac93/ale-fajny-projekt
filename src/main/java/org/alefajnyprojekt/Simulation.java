bfc2b07f939022df8d9d91042d9bd03867b01801
madelajn <madzia.wasik2@gmail.com>
2025-05-14 12:21:57 +0200

    Main modified to start simulation with parameters
    Initials parameters are taken from a JSON file

---
+++ src/main/java/org/alefajnyprojekt/Simulation.java
@@ -0,0 +1,9 @@
+package org.alefajnyprojekt;
+
+public class Simulation {
+     public Simulation(int boardWidth, int boardHeight,
+                       int numHumans, int numRats, int numPets,
+                       int initiallyInfected, int maxTurns) {}
+
+    public void start() {}
+}
